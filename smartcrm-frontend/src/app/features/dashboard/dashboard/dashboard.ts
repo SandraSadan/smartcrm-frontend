da44e5af9185258ee30d05e1d1de0e4f004dcbe5
SandraSadan1 <ss5282g@gre.ac.uk>
2026-08-21 12:49:45 +0530

    feat: add dashboard stats, reusable stat cards, and fixed sidebar and header layout

--- smartcrm-frontend/src/app/features/dashboard/dashboard/dashboard.ts
+++ smartcrm-frontend/src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { StatCard } from '../../../shared/components/stat-card/stat-card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [StatCard],
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  stats = [
+    { title: 'Customers', statValue: '0', icon: 'groups' },
+    { title: 'Leads', statValue: '0', icon: 'trending_up' },
+    { title: 'Tasks', statValue: '0', icon: 'task' },
+    { title: 'Revenue', statValue: '$0', icon: 'payments' },
+  ];
+}
